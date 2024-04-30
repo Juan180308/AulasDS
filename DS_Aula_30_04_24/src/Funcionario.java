725492d0656cbd3e3ce50fc2f317aded1ee97f04
Juan180308 <150930992+Juan180308@users.noreply.github.com>
2024-04-30 09:49:34 -0300

    Add files via upload
    added arquivo da aula

---
+++ DS_Aula_30_04_24/src/Funcionario.java
@@ -0,0 +1,14 @@
+public class Funcionario extends PessoaFisica {
+
+    private String cartao;
+
+    public String setCartao(String cartao) {
+
+        this.cartao = cartao;
+    }
+
+    public String getCartao() {
+
+        return (cartao);
+    }
+}
